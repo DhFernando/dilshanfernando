f04dd577038d2a69f1e0828866e13fd6679c4f3f
Diilshan Fernando <dilshanfe@99x.io>
2023-04-12 22:52:34 +0530

    f

---
+++ src/assets/index.js
@@ -0,0 +1,69 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import blogger from "./blogger.png";
+import mobile from "./mobile.png";
+import distribution from "./distribution.png"; 
+import aws from "./aws.png";
+import web from "./website-codes.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import solidity from "./tech/solidity.svg";
+import serverless from "./tech/serverless.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import angular from "./tech/angular.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import rails from "./tech/rails.png";
+import pulumi from "./tech/pulumi.png";
+import csharp from "./tech/c-sharp.svg";
+
+import meta from "./company/meta.png";
+import writergate from "./company/writergate.jpeg";
+import nem_logo from "./company/nem_logo.jpeg";
+import _99x from "./company/99x.png";
+
+import personalweb from "./personalweb.png";
+import immutablephotogalary from "./immutable-photogalary.png";
+import helpdesk from "./helpdesk.png";
+
+export {
+  logo,
+  backend,
+  blogger,
+  mobile,
+  aws,
+  distribution,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  solidity,
+  serverless,
+  csharp,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  angular,
+  tailwind,
+  typescript,
+  rails,
+  meta,
+  writergate,
+  nem_logo,
+  _99x,
+  personalweb,
+  immutablephotogalary,
+  helpdesk,
+  pulumi
+};
---
+++ src/constants/index.js
@@ -0,0 +1,293 @@
+import {
+    aws, 
+    blogger,
+    web,
+    distribution,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    angular,
+    serverless,
+    nodejs,
+    mongodb,
+    csharp,
+    pulumi,
+    solidity,
+    meta,
+    nem_logo,
+    _99x,
+    writergate,
+    personalweb,
+    immutablephotogalary,
+    helpdesk,
+    rails,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+    {
+      id: "blog",
+      title: "Blog",
+    },
+    {
+      id: "area-51",
+      title: "Area-51",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "AWS Community Builder",
+      icon: aws,
+    },
+    {
+      title: "Blockchain Developer",
+      icon: distribution,
+    },
+    {
+      title: "Content Writer",
+      icon: blogger,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Angular",
+      icon: angular,
+    },
+    {
+      name: "Serverless",
+      icon: serverless,
+    },
+    {
+      name: "Pulumu",
+      icon: pulumi,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Rails",
+      icon: rails,
+    },
+    {
+      name: "C#",
+      icon: csharp,
+    },
+    {
+      name: "Solidity",
+      icon: solidity,
+    },
+
+  ];
+  
+  const experiences = [
+    {
+      title: "System Developer",
+      company_name: "NEM Constructions",
+      icon: nem_logo,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "I worked on a group project for NEM Construction where we developed a complete system for diesel management. During this project, we analyzed the existing system and proposed a fully automated solution utilizing web, mobile, and IoT technologies.",
+      ],
+    },
+    {
+      title: "Software Engineer - Intern  ",
+      company_name: "99x",
+      icon: _99x,
+      iconBg: "#E6DEDD",
+      date: "January 27, 2021 to July 26, 2021",
+      points: [
+        "Contributed to the development of several projects, which has defernt tech-stacks",
+        "Developing and maintaining web applications using React.js, Angular, Serverless and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Software Engineer",
+      company_name: "WriterGate",
+      icon: writergate,
+      iconBg: "#383E56",
+      date: "July 2021 - Feb 2022",
+      points: [
+        "Developing and maintaining web application using React.TS, Pulumi with AWS and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Software Engineer",
+      company_name: "99x",
+      icon: _99x,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using AngularJs + Angular 14, Ruby on Rails and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        `
+        I've worked with Dilshan during his internship period. He is highly committed to the assigned work and can solve problems with minimal supervision. In addition, he is a person who won't give up until the job is completed.
+
+        He has shown the capacity to carry the weight of the project while helping his colleagues and deliver the work on time. 
+
+        Dilshan has the potential to become a valuable asset to any organization.
+
+        I would like to wish him all the very best in his future endeavors
+        `,
+      name: "Ashan Fernando",
+      designation: "Solutions Architecht | Director",
+      company: "99x",
+      image: "https://media.licdn.com/dms/image/C5603AQG_YUMMY-RCkg/profile-displayphoto-shrink_800_800/0/1614136272458?e=1686787200&v=beta&t=z_Au9p-nvWAPxoV5ttiusBgKGbwTZGy1AXadIZECks0",
+      linkedin: "https://www.linkedin.com/in/ashanfer/"
+    },
+    {
+      testimonial:
+        `Dilshan did his internship with one of my teams. When he joined the team, almost all the technologies were new to him. He proved that he is a fast learner and managed to grasp the project technologies, domain knowledge in a very short period of time. It was really easy to work with him as he took ownership of the tasks which are assigned to him. Any team member can depend on his work without any issue. 
+
+        Apart from the technical skills, Dilshan is a good friend to anyone. He always looks out for all the team members. I wish him all the best and I can easily recommend him to any organization.`,
+      name: "Rumesh Hapuarachchi",
+      designation: "Senior Fullstack Developer",
+      company: "Anyfin | Enlear",
+      image: "https://media.licdn.com/dms/image/C5603AQH5qUOI4hIw7w/profile-displayphoto-shrink_100_100/0/1643363781262?e=1686787200&v=beta&t=jXcy9edvCMIlXbSf9__OixJeA4f6to2IGSK8bPcDlyg",
+      linkedin: "https://www.linkedin.com/in/rehrumesh/"
+    },
+    {
+      testimonial:
+        `Dilshan has the ability to quickly grasp what is needed and apply himself fully. He did the language localization for the 99x website with GatsbyJS and Netlify. He also implemented SEO improvements to adapt to the new region. He was committed to his deadlines and clear in his communications. He was also able to step into a presale and add value to build a POC. All the very best Dilshan!`,
+      name: "Chrishan de Mel",
+      designation: "Chief Marketing and Corporate Affairs Officer",
+      company: "99x",
+      image: "https://media.licdn.com/dms/image/C5103AQGydp21hxKG6g/profile-displayphoto-shrink_400_400/0/1516360921741?e=1686787200&v=beta&t=C1QNGytYvQkYpjzY6bvrbUYkvh1I-iRuRBAaWLkBGU8",
+      linkedin: "https://www.linkedin.com/in/chrishan-de-mel/"
+    },
+    {
+      testimonial:
+        `Dilshan was a trainee software engineer at 99x and I worked with him on the project "DevGrade". He is a highly enthusiastic individual who gives the maximum commitment to work. And also he is passionate about technology which gives him the opportunity to learn new technical skills quickly. He is a quick learner with a better vision and mission for the future as well. His delivery on time is perfect and his self-learning abilities in technical areas are beyond the expectation levels. 
+        He is a good team player with good software engineering skills. I highly recommend him to any organization.`,
+      name: "Kavinda Senarathne",
+      designation: "Senior Software Engineer",
+      company: "99x",
+      image: "https://media.licdn.com/dms/image/C5603AQEl6AA8hPY5hA/profile-displayphoto-shrink_100_100/0/1628584546555?e=1686787200&v=beta&t=lhTSzuXhG-T57mjJ6c5XxSmVr2hL0woRpyc3Px_HKPQ",
+      linkedin: "https://www.linkedin.com/in/kavindasenarathne1994/"
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Personal Portfolio",
+      description:
+        `Welcome to my personal portfolio! Discover a unique experience with stunning 3D objects and smooth transitions powered by React and Three.js. Explore my work and skills in an immersive way. Join me on this creative journey!`,
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "threeJs",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: personalweb,
+      source_code_link: "https://github.com/DhFernando/portfolio_",
+    },
+    {
+      name: "Blockchain based - Photo Galary",
+      description: `Experience the future of photo sharing with our immutable gallery powered by Ethereum, IPFS, Web3.js, Metamask, Vue.js, and Solidity. Store and share photos securely with decentralized ownership, transparent records, and user-friendly interface. Join us in the decentralized revolution`,
+      tags: [
+        {
+          name: "VueJs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "IPFS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Web3 | Ethereum",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: immutablephotogalary,
+      source_code_link: "https://github.com/DhFernando/immutable-photo-Galery-Ethereum-Solidity-Web3-IPFS",
+    },
+    {
+      name: "HelpDesk",
+      description: `Introducing our Helpdesk API! Powered by React, .NET Core, and Entity Framework, this app streamlines support ticket management for organizations. With a modern interface and robust backend, it's the ultimate solution for efficient and secure helpdesk operations. Experience the future of support with us on my personal website!`,
+      tags: [
+        {
+          name: "C# / ASP.NET core",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "react",
+          color: "green-text-gradient",
+        },
+        {
+          name: "EntityFramework",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: helpdesk,
+      source_code_link: "https://github.com/DhFernando/helpdesk",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
